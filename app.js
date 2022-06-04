be8362c68ecafa49e90d8df6292405a39e27fa2b
HedgehogKU <ccc908925@gmail.com>
2022-06-04 16:03:24 +0800

    ADD: catch err name: CastError

--- app.js
+++ app.js
@@ -107,6 +107,13 @@ app.use((err, req, res, next) => {
         return resErrorProd(err, res);
     }
 
+    if ( err.name === 'CastError' ) {
+        err.message = '傳入的值與伺服器定義型別有誤';
+        err.statusCode = 400;
+        err.isOperational = true;
+        return resErrorProd(err, res);
+    }
+
     resErrorProd(err, res);
 });
 
