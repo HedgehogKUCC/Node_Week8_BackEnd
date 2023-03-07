d6af91b195cbd6345d12b15c2883f28f05f9050d
ChanCheng KU <ccc908925@gmail.com>
2023-03-07 14:22:34 +0800

    ADD: environment in swagger.ts

--- swagger.ts
+++ swagger.ts
@@ -2,14 +2,24 @@ import swaggerAutogen from 'swagger-autogen';
 
 const outputFile = './dist/swagger_output.json';
 const endpointsFiles = ['./app.ts'];
+let host = '';
+let schemes: string[];
+
+if ( process.env.NODE_ENV === 'development' ) {
+  host = 'localhost:3005';
+  schemes = ['http'];
+} else {
+  host = 'https://meta-wall-pi.vercel.app';
+  schemes = ['https'];
+}
 
 const doc = {
   info: {
     title: 'Node.js REST API',
     description: 'Node.js 直播班'
   },
-  host: 'localhost:3005',
-  schemes: ['http', 'https'],
+  host,
+  schemes,
   tags: [
     {
       name: 'Users',
