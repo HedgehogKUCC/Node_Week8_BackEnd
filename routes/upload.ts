634ba471cf29db0b871bd7fe397034ed60d58246
ChanCheng KU <ccc908925@gmail.com>
2023-03-07 14:21:26 +0800

    DOC: POST '/upload'

--- routes/upload.ts
+++ routes/upload.ts
@@ -8,6 +8,18 @@ import UploadController from '../controllers/UploadController';
 import isAuth from '../middlewares/isAuth';
 import imgurUpload from '../middlewares/imgurUpload';
 
-router.post('/', isAuth, imgurUpload, handleErrorAsync(UploadController.upload));
+router.post('/', isAuth, imgurUpload, handleErrorAsync(
+  /*
+    #swagger.tags = ['Others']
+    #swagger.consumes = ['multipart/form-data']
+    #swagger.parameters['avatar'] = {
+      in: 'formData',
+      type: 'file',
+      required: 'true',
+      description: '上傳用戶大頭貼',
+    }
+   */
+  UploadController.upload
+));
 
 export default router;
