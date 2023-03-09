a8f86ca037dc2b138ece9eb8ddb8a23775ccf517
ChanCheng KU <ccc908925@gmail.com>
2023-03-09 15:50:21 +0800

    UPDATE: swagger.ts

--- swagger.ts
+++ swagger.ts
@@ -3,14 +3,27 @@ import swaggerAutogen from 'swagger-autogen';
 const outputFile = './dist/swagger_output.json';
 const endpointsFiles = ['./app.ts'];
 let host = '';
-let schemes: string[];
+let servers: {
+  url: string;
+  description: string;
+}[];
 
 if ( process.env.NODE_ENV === 'development' ) {
   host = 'localhost:3005';
-  schemes = ['http'];
+  servers = [
+    {
+      url: `http://${host}`,
+      description: 'local server'
+    }
+  ];
 } else {
-  host = 'https://meta-wall-pi.vercel.app';
-  schemes = ['https'];
+  host = 'meta-wall-pi.vercel.app';
+  servers = [
+    {
+      url: `https://${host}`,
+      description: '(production) vercel server'
+    }
+  ]
 }
 
 const doc = {
@@ -18,8 +31,9 @@ const doc = {
     title: 'Node.js REST API',
     description: 'Node.js 直播班'
   },
+  servers,
   host,
-  schemes,
+  schemes: ['http', 'https'],
   tags: [
     {
       name: 'Users',
@@ -34,6 +48,80 @@ const doc = {
       description: '其它功能'
     }
   ],
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT'
+    }
+  },
+  definitions: {
+    ResponseServerErrorMsg: {
+      result: false,
+      msg: '系統錯誤，請洽系統管理員'
+    },
+    ResponseErrorMsg: {
+      result: false,
+      msg: '',
+      columns: {
+        Property: {
+          name: 'ValidatorError',
+          message: '',
+          properties: {
+            message: '',
+            type: '',
+            enumValues: [],
+            path: '',
+            value: '',
+          },
+          kind: '',
+          path: '',
+          value: '',
+        }
+      }
+    },
+    ResponseNotFoundPage: {
+      result: false,
+      msg: '無此路由'
+    },
+    ResponseInvalidToken: {
+      result: false,
+      msg: '請重新登入帳號'
+    },
+    SignUp: {
+      $name: '',
+      $sex: '[male | female]',
+      $email: '',
+      $password: '',
+      $confirmPassword: '',
+    },
+    SignUpSuccess: {
+      result: true,
+      data: {
+        user: {
+          token: '',
+          name: '',
+        }
+      }
+    },
+    SignIn: {
+      $email: '',
+      $password: '',
+    },
+    SignInSuccess: {
+      result: true,
+      data: {
+        user: {
+          token: '',
+          name: '',
+        }
+      }
+    },
+    UploadImageSuccess: {
+      result: true,
+      data: '',
+    }
+  }
 }
 
-swaggerAutogen()(outputFile, endpointsFiles, doc);
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
