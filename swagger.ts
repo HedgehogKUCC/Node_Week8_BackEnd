37b1b82e4a656767248c69bf1f6ac96b1f7e3244
ChanCheng KU <ccc908925@gmail.com>
2023-03-10 13:27:44 +0800

    FIX: enumValues

--- swagger.ts
+++ swagger.ts
@@ -74,7 +74,7 @@ const doc = {
           properties: {
             message: '',
             type: '',
-            enumValues: [],
+            enumValues: [ '' ],
             path: '',
             value: '',
           },
